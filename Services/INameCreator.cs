ecdfa29f877f9211ac720e4b34d7f5c3f0bb2233
Serhii Amelin <amserega90@gmail.com>
2023-04-14 12:26:48 +0300

    to PR

--- Services/INameCreator.cs
+++ Services/INameCreator.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using LoggerApp.Services;
 
-namespace LoggerApp
+namespace LoggerApp.Services
 {
     public interface INameCreator
     {
